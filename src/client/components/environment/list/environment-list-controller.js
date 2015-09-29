8af1e766f09c0551b0af4fa2803b02b059e452dc
Ian D. McCunn <imccunn@expedia.com>
2015-09-29 15:49:24 -0700

    environment page and controller update

--- src/client/components/environment/list/environment-list-controller.js
+++ src/client/components/environment/list/environment-list-controller.js
@@ -2,5 +2,18 @@
 var listController = require('../../util/list-controller');
 
 module.exports = function(app) {
-  app.controller('EnvironmentListController', listController('Environments', '/v2/environments'));
+  app.controller('EnvironmentListController', EnvironmentListController);
+
+  /*@ngInject*/
+  function EnvironmentListController($scope, $http) {
+    $http.get('http://localhost:8080/environments')
+      .then(function(res) {
+        $scope.listStatus = 'loaded';
+        console.log(res);
+        $scope.items = res.data._embedded.environments;
+      }, function(err) {
+        $scope.listStatus = 'error';
+        console.log(err);
+      });
+  }
 };
