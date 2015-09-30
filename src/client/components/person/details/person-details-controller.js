f25ba01065047e1407c5216da00d2fc34780c1d9
Ian D. McCunn <imccunn@expedia.com>
2015-09-30 09:46:10 -0700

    remove logs

--- src/client/components/person/details/person-details-controller.js
+++ src/client/components/person/details/person-details-controller.js
@@ -4,18 +4,15 @@ module.exports = function(app) {
 
   app.controller('PersonDetailsController', PersonDetailsController);
 
-  /*ngInject*/
+  /*@ngInject*/
   function PersonDetailsController($scope, $http, $routeParams) {
 
     var successHandler = function(res) {
-      console.log(res.data);
-      var fullName = $scope.displayName(res.data);
       $scope.model.page.title = pageTitle(fullName);
       $scope.person = res.data;
-      $scope.person.firstNameLastName = fullName;
+      $scope.person.firstNameLastName = $scope.displayName(res.data);;
       $http.get(res.data._links.directReports.href)
         .then(function(res) {
-          console.log('direct reps: ', res);
           $scope.person.directReports = res.data._embedded.persons;
           for (var i = 0; i < $scope.person.directReports.length; i++) {
             $scope.person.directReports[i].displayName = $scope.displayName($scope.person.directReports[i]);
@@ -25,7 +22,6 @@ module.exports = function(app) {
         });
       $http.get(res.data._links.manager.href)
         .then(function(res) {
-          console.log(res);
           $scope.person.manager = res.data;
           $scope.person.manager.displayName = $scope.displayName($scope.person.manager);
         }, function(res) {
