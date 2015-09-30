0cce2c15ce7fefbd54324760d14582e126f0d679
Ian D. McCunn <imccunn@expedia.com>
2015-09-30 09:51:20 -0700

    projection for si page

--- src/client/components/service-instance/list/service-instance-list-controller.js
+++ src/client/components/service-instance/list/service-instance-list-controller.js
@@ -1,5 +1,5 @@
 var pagingController = require('../../util/paging-controller');
 
 module.exports = function(app) {
-	app.controller('ServiceInstanceListController', pagingController('Service Instances', 'http://localhost:8080/serviceInstances', 'key'));
+	app.controller('ServiceInstanceListController', pagingController('Service Instances', 'http://localhost:8080/serviceInstances?projection=serviceServiceInstances&', 'key'));
 };
