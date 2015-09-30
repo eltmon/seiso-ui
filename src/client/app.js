5db43ce50ffe8b134e2a978da5f918a119b5d52d
Ian D. McCunn <imccunn@expedia.com>
2015-09-30 09:45:26 -0700

    remove extentions from require statements

--- src/client/app.js
+++ src/client/app.js
@@ -22,100 +22,100 @@ var app = angular.module('seiso', ['ngRoute', 'ngSanitize', 'seisoFilters', 'sei
  */
 
 // General
-require('./components/home/home-controller.js')(app);
-require('./components/global/globals-controller.js')(app);
-require('./components/auth/login-controller.js')(app);
-require('./components/util/search-controller.js')(app);
-require('./components/admin/admin-controller.js')(app);
+require('./components/home/home-controller')(app);
+require('./components/global/globals-controller')(app);
+require('./components/auth/login-controller')(app);
+require('./components/util/search-controller')(app);
+require('./components/admin/admin-controller')(app);
 
 // Eos Actions
-require('./components/service-instance/details/eos-actions/eos-convict-controller.js')(app);
-require('./components/service-instance/details/eos-actions/eos-deploy-controller.js')(app);
-require('./components/service-instance/details/eos-actions/eos-interrogate-controller.js')(app);
-require('./components/service-instance/details/eos-actions/eos-maintenance-mode-controller.js')(app);
-require('./components/service-instance/details/eos-actions/eos-reload-controller.js')(app);
-require('./components/service-instance/details/eos-actions/eos-set-active-controller.js')(app);
-require('./components/service-instance/details/eos-actions/eos-soak-controller.js')(app);
+require('./components/service-instance/details/eos-actions/eos-convict-controller')(app);
+require('./components/service-instance/details/eos-actions/eos-deploy-controller')(app);
+require('./components/service-instance/details/eos-actions/eos-interrogate-controller')(app);
+require('./components/service-instance/details/eos-actions/eos-maintenance-mode-controller')(app);
+require('./components/service-instance/details/eos-actions/eos-reload-controller')(app);
+require('./components/service-instance/details/eos-actions/eos-set-active-controller')(app);
+require('./components/service-instance/details/eos-actions/eos-soak-controller')(app);
 
 // Data-center
-require('./components/data-center/details/data-center-details-controller.js')(app);
-require('./components/data-center/list/data-center-list-controller.js')(app);
+require('./components/data-center/details/data-center-details-controller')(app);
+require('./components/data-center/list/data-center-list-controller')(app);
 
 // Environment
-require('./components/environment/details/environment-details-controller.js')(app);
-require('./components/environment/list/environment-list-controller.js')(app);
+require('./components/environment/details/environment-details-controller')(app);
+require('./components/environment/list/environment-list-controller')(app);
 
 // Load Balancer
-require('./components/load-balancer/details/load-balancer-details-controller.js')(app);
-require('./components/load-balancer/list/load-balancer-list-controller.js')(app);
+require('./components/load-balancer/details/load-balancer-details-controller')(app);
+require('./components/load-balancer/list/load-balancer-list-controller')(app);
 
 // Machine
-require('./components/machine/machine-details-controller.js')(app);
+require('./components/machine/machine-details-controller')(app);
 
 // Person
 
-require('./components/person/details/person-details-controller.js')(app);
-require('./components/person/list/person-list-controller.js')(app);
+require('./components/person/details/person-details-controller')(app);
+require('./components/person/list/person-list-controller')(app);
 
 // Service Instance
-require('./components/service-instance/details/service-instance-details-controller.js')(app);
-require('./components/service-instance/details/nodes/nodes-controller.js')(app);
-require('./components/service-instance/details/dependencies/service-instance-dependencies-controller.js')(app);
-require('./components/service-instance/details/dependencies/service-instance-dependents-controller.js')(app);
-require('./components/service-instance/list/service-instance-list-controller.js')(app);
+require('./components/service-instance/details/service-instance-details-controller')(app);
+require('./components/service-instance/details/nodes/nodes-controller')(app);
+require('./components/service-instance/details/dependencies/service-instance-dependencies-controller')(app);
+require('./components/service-instance/details/dependencies/service-instance-dependents-controller')(app);
+require('./components/service-instance/list/service-instance-list-controller')(app);
 
-require('./components/person/details/person-details-controller.js')(app);
-require('./components/person/list/person-list-controller.js')(app);
+require('./components/person/details/person-details-controller')(app);
+require('./components/person/list/person-list-controller')(app);
 
 // Service Instance
-require('./components/service-instance/details/service-instance-details-controller.js')(app);
-require('./components/service-instance/details/nodes/nodes-controller.js')(app);
-require('./components/service-instance/details/dependencies/service-instance-dependencies-controller.js')(app);
-require('./components/service-instance/details/dependencies/service-instance-dependents-controller.js')(app);
-require('./components/service-instance/list/service-instance-list-controller.js')(app);
+require('./components/service-instance/details/service-instance-details-controller')(app);
+require('./components/service-instance/details/nodes/nodes-controller')(app);
+require('./components/service-instance/details/dependencies/service-instance-dependencies-controller')(app);
+require('./components/service-instance/details/dependencies/service-instance-dependents-controller')(app);
+require('./components/service-instance/list/service-instance-list-controller')(app);
 
 // Nodes
-require('./components/service-instance/details/dashboard/node-alerts-controller.js')(app);
-require('./components/service-instance/details/dashboard/node-breakdown-controller.js')(app);
-require('./components/service-instance/details/dashboard/node-summary-controller.js')(app);
-require('./components/service-instance/details/dashboard/node-details-controller.js')(app);
+require('./components/service-instance/details/dashboard/node-alerts-controller')(app);
+require('./components/service-instance/details/dashboard/node-breakdown-controller')(app);
+require('./components/service-instance/details/dashboard/node-summary-controller')(app);
+require('./components/service-instance/details/dashboard/node-details-controller')(app);
 
 // Service
-require('./components/service/details/service-details-controller.js')(app);
-require('./components/service/details/service-instances-controller.js')(app);
-require('./components/service/details/service-documentation-controller.js')(app);
-require('./components/service/list/service-list-controller.js')(app);
+require('./components/service/details/service-details-controller')(app);
+require('./components/service/details/service-instances-controller')(app);
+require('./components/service/details/service-documentation-controller')(app);
+require('./components/service/list/service-list-controller')(app);
 
 // Other
-require('./components/status/status-list-controller.js')(app);
-require('./components/type/type-list-controller.js')(app);
+require('./components/status/status-list-controller')(app);
+require('./components/type/type-list-controller')(app);
 
 // MB
-require('./components/mb/mb-controller.js')(app);
+require('./components/mb/mb-controller')(app);
 
 /**
  *    Services
  */
-require('./components/dataService/dataService.service.js')(app);
-require('./components/auth/auth.service.js')(app);
-require('./components/util/ng-services.js')(angular);
+require('./components/dataService/dataService.service')(app);
+require('./components/auth/auth.service')(app);
+require('./components/util/ng-services')(angular);
 
 /**
  *    Constants
  */
-require('./components/util/ng-constants.js')(app);
+require('./components/util/ng-constants')(app);
 
-require('./components/util/ng-directives.js')(app);
+require('./components/util/ng-directives')(app);
 
-require('./components/util/ng-filters.js')(angular);
+require('./components/util/ng-filters')(angular);
 
 /**
  *    Config
  */
-require('./app.config.js')(app);
-require('./app.routes.js')(app);
+require('./app.config')(app);
+require('./app.routes')(app);
 
 /**
  *    Run
  */
-require('./app.run.js')(app);
+require('./app.run')(app);
