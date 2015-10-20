cbcedcb9b98c6de57d4b5bd88b1d061cb0294c19
Willie Wheeler <wwheeler@expedia.com>
2015-10-20 13:44:56 -0700

    Fixed reload error

--- gulpfile.js/lib/getEnabledTasks.js
+++ gulpfile.js/lib/getEnabledTasks.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var config = require('../config');
-//var compact = require('lodash/array/compact');
+var compact = require('lodash/array/compact');
 
 // Grouped by what can run in parallel.
 // Really I think all of these can run in parallel. Collapse? [WLW]
@@ -11,7 +11,7 @@ var codeTasks = ['bs:css', 'bs:js', 'html', 'jquery', 'less', 'webpack'];
 
 module.exports = function(env) {
   return {
-    assetTasks: assetTasks,
-    codeTasks: codeTasks
+    assetTasks: compact(assetTasks),
+    codeTasks: compact(codeTasks)
   };
 };
--- gulpfile.js/tasks/serve.js
+++ gulpfile.js/tasks/serve.js
@@ -8,11 +8,15 @@ function task() {
   var watchPath = config.paths.client + '/**';
 
   browserSync.init({
-    watch: watchPath,
-    server: config.paths.out
+    server: {
+      baseDir: config.paths.out
+    }
   });
 
-  gulp.watch([watchPath], ['watch:reload']);
+  // FIXME ['watch:reload'] is causing an error when I save a content change:
+  // "Task '[object Object]' is not in your gulpfile"
+  // Looks like it's because watch:reload is calling build
+  gulp.watch(watchPath, ['watch:reload']);
 }
 
 gulp.task('serve', ['build'], task);
