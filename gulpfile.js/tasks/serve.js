439a566de91ade702673e175341c6e6c3902a2ac
Willie Wheeler <wwheeler@expedia.com>
2015-10-20 13:46:21 -0700

    Removed outdated comments

--- gulpfile.js/tasks/serve.js
+++ gulpfile.js/tasks/serve.js
@@ -13,9 +13,6 @@ function task() {
     }
   });
 
-  // FIXME ['watch:reload'] is causing an error when I save a content change:
-  // "Task '[object Object]' is not in your gulpfile"
-  // Looks like it's because watch:reload is calling build
   gulp.watch(watchPath, ['watch:reload']);
 }
 
