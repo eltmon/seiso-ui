2e6208fbe5647f52fb353a6e44dcce26f9ac7d8d
Ian D. McCunn <imccunn@expedia.com>
2015-11-04 10:47:30 -0800

    auth route fix; remove old route config

--- src/client/app.routes.js
+++ src/client/app.routes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     $stateProvider
       .state('home', stateConfig('/', 'Home', 'home/home'))
       .state('search', stateConfig('/search', 'Search', 'search'))
-      .state('login', stateConfig('/login', 'Login', 'admin/login'))
+      .state('login', stateConfig('/login', 'Login', 'auth/login'))
       .state('admin', stateConfig('/admin', 'Admin', 'admin/index'))
       .state('mb', viewRoute('/mb', 'mb/index'))
       .state('mb:type', viewRoute('/mb:type', 'mb/profile'))
@@ -27,30 +27,6 @@ module.exports = function(app) {
       .state('serviceInstance', viewRoute('/service-instances/:key', 'service-instance/details/service-instance-details'))
       .state('statuses', stateConfig('/statuses', 'StatusList', 'status/status-list'))
       .state('types', stateConfig('/types', 'TypeList', 'type/type-list'));
-    // $routeProvider
-    //   .when('/', route('Home', 'home/home'))
-    //   .when('/search', route('Search', 'search'))
-    //   .when('/login', route('Login', 'auth/login'))
-    //   .when('/admin', route('Admin', 'admin/index'))
-    //   .when('/mb', viewRoute('mb/index'))
-    //   .when('/mb/:type', viewRoute('mb/profile'))
-    //   .when('/data-centers', viewRoute('data-center/list/data-center-list'))
-    //   .when('/data-centers/:key', route('DataCenterDetails', 'data-center/details/data-center-details'))
-    //   .when('/environments', route('EnvironmentList', 'environment/list/environment-list'))
-    //   .when('/environments/:key', route('EnvironmentDetails', 'environment/details/environment-details')) 
-    //   .when('/load-balancers', route('LoadBalancerList', 'load-balancer/list/load-balancer-list'))
-    //   .when('/load-balancers/:name', route('LoadBalancerDetails', 'load-balancer/details/load-balancer-details'))
-    //   .when('/machines/:name', route('MachineDetails', 'machine/machine-details'))
-    //   .when('/nodes/:name', route('NodeDetails', 'service-instance/details/nodes/details/node-details'))
-    //   .when('/people', route('PersonList', 'person/list/person-list'))
-    //   .when('/people/:username', route('PersonDetails', 'person/details/person-details'))
-    //   .when('/services', route('ServiceList', 'service/list/service-list'))
-    //   .when('/services/:key', viewRoute('service/details/service-details'))
-    //   .when('/service-instances', route('ServiceInstanceList', 'service-instance/list/service-instance-list'))
-    //   .when('/service-instances/:key', viewRoute('service-instance/details/service-instance-details'))
-    //   .when('/statuses', route('StatusList', 'status/status-list'))
-    //   .when('/types', route('TypeList', 'type/type-list'))
-    //   .otherwise({ redirectTo : '/' });
 
     function stateConfig(url, controllerName, viewName) {
       return {
