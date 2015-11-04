ec908256714859608db5918f2bdc6c7043c877ae
Ian D. McCunn <imccunn@expedia.com>
2015-11-04 10:52:25 -0800

    jquery being injected in incorrect location

--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,26 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: {
-    build: './src/client/app.js',
-    bootstrap: './node_modules/bootstrap/dist/js/bootstrap.min.js',
     // jquery: './node_modules/jquery/dist/jquery.min.js',
-    material: './node_modules/angular-material/angular-material.min.js'
+    bootstrap: './node_modules/bootstrap/dist/js/bootstrap.min.js',
+    material: './node_modules/angular-material/angular-material.min.js',
+    build: './src/client/app.js'
   },
   output: {
     path: path.resolve(__dirname, 'static/js'),
     filename: '[name].bundle.js',
     chunkFileName: '[id].bundle.js'
   },
+  externals: {
+    'jquery': 'jQuery'
+  },
   plugins: [
     new webpack.ProvidePlugin({
       // Automtically detect jQuery and $ as free var in modules
       // and inject the jquery library
       // This is required by many jquery plugins
-      jQuery: "jquery"
+      // jQuery: "jquery",
+      $: 'jquery'
     })
   ]
 };
