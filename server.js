e69473a3f2fbad498124ae36dc2fff682d5b6f7e
Ian D. McCunn <imccunn@expedia.com>
2016-01-14 16:11:47 -0800

    cleanup, documentation, removing hardcode of server point so config file can be used

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
+/**
+ * seiso-ui Web Server
+ *
+ * Serves static content of static ui with configuration
+ * for service endpoints (seiso-api, etc).
+ *
+ * Author: Ian McCunn <imccunn@expedia.com>
+ * 
+ */
+
 'use strict';
 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var logger = require('morgan');
-var config =  require('./config');
-var logConfig = require('./lib/logging');
-var apiEndpointCtrl = require('./lib/controllers').apiEndpoint;
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	logger = require('morgan'),
+	config =  require('./config'),
+	logConfig = require('./lib/logging'),
+	apiEndpointCtrl = require('./lib/controllers').apiEndpoint,
+	publicDir = __dirname + '/static';
 
-var port = process.env.PORT || 3000;
-var publicDir = __dirname + '/static';
 
 logger.format('access', logConfig.loggerFormat);
 app.use(logger('access', {stream: logConfig.accessLogStream}));
 
-app.set('port', port);
+app.set('port', config.port);
 app.use('/', express.static(publicDir));
 
 app.use('/getApiConfig', apiEndpointCtrl);
 
 function start() {
-  app.listen(app.get('port'), function() {
+  app.listen(config.port, function() {
     console.log('Server listening on port ', app.get('port'));
+    console.log('Config: \n', config);
   });
 }
 
