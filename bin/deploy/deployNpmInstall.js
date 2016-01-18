c6e9b91632291e86f626b96386e47e1ff613101a
Ian D. McCunn <imccunn@expedia.com>
2016-01-17 16:44:41 -0800

    adding documentation to build/deploy script

--- bin/deploy/deployNpmInstall.js
+++ bin/deploy/deployNpmInstall.js
@@ -19,6 +19,8 @@ var fs = require('fs'),
     exclusions; // to be set after file is read.
 console.log(args);
 
+// Fail if any number of args are not passed.
+// TODO: Use arguments handling package or create ability for optional flags. [IDM]
 for (var i = 0; i < args.length; i++) {
   if (args[i] == undefined || args[i] == '') {
     console.log('bad arg: ', args[i]);
@@ -28,6 +30,7 @@ for (var i = 0; i < args.length; i++) {
   }
 }
 
+// Optionally create a backup/copy of original package.json
 console.log('Installing deployment packages');
 if (createBackup === 'true') {
   console.log('creating backup of package.json');
@@ -37,6 +40,8 @@ if (createBackup === 'true') {
   });
 }
 
+// Read in exclusions file of list of package names and create array of dependency names.
+// This assumes they are spelled according to npm registry name.
 if (!exclusionsFile) {
   console.log('No package exclusions specified.');
   console.log('Specify a newline delimited text file of package names to exlude from npm install');
@@ -49,34 +54,43 @@ if (!exclusionsFile) {
   });
 }
 
+// Read package.json dependencies list.
 fs.readFile(packageFile, 'utf-8', function(err, data) {
   if (err) return console.log(err);
   var originalPackage = JSON.parse(data),
       deployPackage = originalPackage;
-
+  
+  // Remove dependency from dependencies object if matched by exclusions list.
   for (var packKey in deployPackage.devDependencies) {
     if (exclusions.indexOf(packKey) !== -1) {
       delete deployPackage.devDependencies[packKey];
     }
   }
+  // Remove original package.json.
   fs.unlink(packageFile, function(err) {
     if (err) return console.log(err);
+    
+    // Write package.json with dependencies list which has exclusions removed. 
     fs.writeFile(packageFile, JSON.stringify(deployPackage, null, 2), 'utf-8', function(err) {
       if (err) return console.log(err);
-      console.log('Installing packages...');
+
+      // Run 'npm install' with deployment dependencies
       exec('npm install', function(err, stdout, stderr) {
         if (err) return console.log(err);
         console.log(stdout, stderr);
-        if (preservePackage) {
-          fs.writeFile('package.json', JSON.stringify(originalPackage, null, 2), function(err) {
+
+        // Optionally reinstate original package.json.
+        if (preservePackage) { 
+          fs.writeFile('package.json', JSON.stringify(originalPackage, null, 2), function(err) { 
             if (err) return console.log(err);
             console.log('Original package.json restored.');
           }) 
         } else {
+          // Otherwise remove package.json from deployment artifact.
           fs.unlink('package.json', function(err) {
             if (err) return console.log(err);
             console.log('Deploy packages installed. package.json removed.');
-          })
+          });
         }
       });
     });
