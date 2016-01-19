32eae7aa33d8ddcfc25ec33854d4e3b3d6bbcfd4
Ian D. McCunn <imccunn@expedia.com>
2016-01-19 10:45:30 -0800

    removing references to resources in packages since these can change

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -4,18 +4,13 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')({ lazy: true });
 var config = require('../config');
 
-function task() {
+function fontsTask() {
   var exts = '*.{eot,svg,ttf,woff,woff2}';
   
   gulp.src(config.client + '/css/fonts/' + exts)
       .pipe(gulp.dest(config.out + '/fonts'));
 
-  gulp.src(config.nodeModules + '/font-awesome/fonts/' + exts)
-      .pipe(gulp.dest(config.out + '/fonts')); 
-
-  gulp.src(config.nodeModules + '/bootstrap/fonts/' + exts)
-      .pipe(gulp.dest(config.out + '/css/fonts'));
 }
 
-gulp.task('fonts', task);
-module.exports = task;
+gulp.task('fonts', ['clean'], fontsTask);
+module.exports = fontsTask;
