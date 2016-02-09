b614bc3a002b2a51d1737938beed4aeb15216c4f
Ian D. McCunn <imccunn@expedia.com>
2016-02-09 11:05:05 -0800

    updating mechanism by which dependents and dependencies are fetched, removing this tab for now

--- src/client/components/service-instance/details/dependencies/service-instance-dependents-controller.js
+++ src/client/components/service-instance/details/dependencies/service-instance-dependents-controller.js
@@ -6,15 +6,19 @@ module.exports = function(app) {
   function serviceInstanceDependentsController($scope, dataService, $stateParams) {
     $scope.dependentsStatus = 'loading';
     var siKey = $stateParams.key;
-    var path = '/serviceInstanceDependents/search/findByDependentKey?key=' + siKey;
+    var path = '/serviceInstances/search/findByName?Name=' + siKey;
     var successHandler = function(res) {
-      console.log(res);
-      $scope.dependents = res.data._embedded.serviceInstanceDependencies;
-      $scope.dependentsStatus = 'loaded';
+      dataService.get(res.data._links.dependents.href)
+        .then(function(res) {
+          $scope.dependents = res.data._embedded.serviceInstanceDependencies;
+          $scope.dependentsStatus = 'loaded';
+        });
+
     };
     var errorHandler = function() {
       $scope.dependentsStatus = 'error';
     };
+    
     dataService.get(path)
       .then(successHandler, errorHandler);
   }
