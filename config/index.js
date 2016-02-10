3b16f45ab70736a9f2d86a625b7d33d2f57528d1
Ian D. McCunn <imccunn@expedia.com>
2016-02-10 09:55:11 -0800

    adding env config

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@
 var _ = require('lodash');
 
 var all = require('./env/all.js');
+var envConfig = require('./env/' + (process.env.NODE_ENV || 'dev') + '.js') || {};
 var chefBuildConfig = {};
 
 try {
@@ -11,4 +12,4 @@ try {
 	console.log('Error reading config file: ', e);
 }
 
-module.exports = _.extend(all, chefBuildConfig);
+module.exports = _.extend(all, envConfig, chefBuildConfig);
