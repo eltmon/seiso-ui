493f78e89beca75d4b0a05ba57fdf0fd5c0a00d1
Ian D. McCunn <imccunn@expedia.com>
2016-02-10 16:45:37 -0800

    adding env specific config files and auth config

--- config/config.js
+++ config/config.js
@@ -1,22 +1,22 @@
 
 module.exports = {
-	instances: {
-		self: {
-			name: 'Sandbox',
-			href: 'seiso-ui-sandbox.example.com'
-		},
-		env: {
-			test: {
-				name: 'Test',
-				href: 'https://thisapp.test.example.com'
-			},
-			prod: {
-				name: 'Prod',
-				href: 'https://thisapp.prod.example.com'
-			}
-		}
-	},
-	apis: {
-		seiso: 'http://localhost:8080'
-	}
+  instances: {
+    self: {
+      name: 'Sandbox',
+      href: 'seiso-ui-sandbox.example.com'
+    },
+    env: {
+      test: {
+        name: 'Test',
+        href: 'https://thisapp.test.example.com'
+      },
+      prod: {
+        name: 'Prod',
+        href: 'https://thisapp.prod.example.com'
+      }
+    }
+  },
+  apis: {
+    seiso: 'http://localhost:8080'
+  }
 };
--- config/env/all.js
+++ config/env/all.js
@@ -1,8 +1,28 @@
 
 module.exports = {
+  applicationName: '',
   accessLog: {
     fileSize: '1m',
     keep: 10,
     compress: true
-  }
+  },
+  auth: {
+    secured: (process.env.NODE_ENV === 'dev' ? false : true),
+    strategy: 'saml',
+    acceptableClockSkewInMs: 360000,
+    callbackProtocol: 'https',
+    identityProvider: {
+      name: '',
+      identity: '',
+      spInitiatedUrl: '',
+      signingCert: ''
+    },
+    serviceProvider: {
+      name: '',
+      identity: '',
+      desiredSubjectFormat: 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified'
+    }
+  },
+  hostingEnvironment: '',
+  seissionSecret: 'secret'
 };
