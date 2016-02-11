c666b19a55ab0ed71214afd5d99a56f88026017b
Ian D. McCunn <imccunn@expedia.com>
2016-02-10 17:08:07 -0800

    cleanup

--- src/server/routes.js
+++ src/server/routes.js
@@ -3,12 +3,6 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
 var homeController = require('./controllers/home');
-// var apiVersionController = require('./controllers/api/version');
-// var apiSeisoController = require('./controllers/api/seiso');
-// var apiPostController = require('./controllers/api/filedata');
-// var apiMachineQueryController = require('./controllers/api/machineQuery');
-// var apiSmartlabController = require('./controllers/api/getsmart');
-// var apiServiceInstancesController = require('./controllers/api/serviceInstances');
 
 /**
  * Initializes and secures routes
@@ -51,32 +45,4 @@ exports.init = function initializeRoutes(app, passport, authenticationStrategy, 
 	app.get('/index(.(html|htm|js|php|aspx))?', function(req, res) {
 		res.redirect('/');
 	});
-
-	// app.get('/', homeController.index);
-
-  // API
-  // app.post('/api', jsonParser, apiPostController.postServiceInstance);
-  // app.post('/api/machine-query', jsonParser, apiMachineQueryController.queryMachineList);
-  // app.get('/api/machine-query', apiMachineQueryController.requestMachineResults);
-  // app.get('/api/version', apiVersionController.getCurrent);
-  // app.get('/api/version/current', apiVersionController.getCurrent);
-  // app.post('/api/getsmart/load-balancer/machine/', apiSmartlabController.queryIdLoadBalancer);
-  // app.post('/api/getsmart/load-balancer-names', apiSmartlabController.getLoadBalancerApiHandles);
-  // app.post('/api/service-instances', apiServiceInstancesController.post);
-  // app.get('/api/seiso/services', apiSeisoController.getServiceList);
-  // app.get('/api/seiso/services/:key', apiSeisoController.getService);
-  // app.get('/api/seiso/service-types', apiSeisoController.getTypeList);
-  // app.get('/api/seiso/service-types/:key', apiSeisoController.getType);
-  // app.get('/api/seiso/data-centers', apiSeisoController.getDatacenterList);
-  // app.get('/api/seiso/data-centers/:key', apiSeisoController.getDatacenter);
-  // app.get('/api/seiso/environments', apiSeisoController.getEnvironmentList);
-  // app.get('/api/seiso/environments/:key', apiSeisoController.getEnvironment);
-  // app.get('/api/seiso/load-balancers', apiSeisoController.getLoadbalancerList);
-  // app.get('/api/seiso/load-balancers/:key', apiSeisoController.getLoadbalancer);
-  // app.get('/api/seiso/service-instances', apiSeisoController.getServiceInstanceList);
-  // app.get('/api/seiso/service-instances/:key', apiSeisoController.getServiceInstance);
-  // app.get('/api/seiso/nodes', apiSeisoController.getNodeList);
-  // app.get('/api/seiso/nodes/:key', apiSeisoController.getNodeInstance);
-  // app.get('/api/seiso/machines', apiSeisoController.getMachineList);
-  // app.get('/api/seiso/machines/:key', apiSeisoController.getMachineInstance);
 }
