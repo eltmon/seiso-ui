918ae1b9d39fc6113189460692699e3505290e36
Ian D. McCunn <imccunn@expedia.com>
2016-02-21 13:35:57 -0800

    convert to vm convention

--- client/components/service-instance/details/nodes/nodes-controller.js
+++ client/components/service-instance/details/nodes/nodes-controller.js
@@ -6,60 +6,61 @@ var async = require('async'),
 module.exports = function(app) {
   app.controller('ServiceInstanceNodesController', serviceInstanceNodesController);
 
-  /* @ngInject */  
+  /* @ngInject */ 
   function serviceInstanceNodesController(dataService, paginationConfig, $stateParams) {
     var vm = this;
-    vm.nodes = {
-      currentPage: 1,
-      pageSelected: function() {
-        vm.nodeListStatus = 'loading';
-        var pageNumber = vm.nodes.currentPage;
-        var apiPageNumber = pageNumber - 1;
-        var requestUrl = '/nodes/search/findByServiceInstanceKey?key=' + $stateParams.key +
-            '&page=' + apiPageNumber +
-            '&size=' + paginationConfig.itemsPerPage +
-            '&sort=name' +
-            '&projection=serviceInstanceNodes';
+    vm.currentPage = 1;
+    vm.pageSelected = function() {
+      console.log('thing');
+      console.log(vm.currentPage);
+      vm.nodeListStatus = 'loading';
+      var pageNumber = vm.currentPage;
+      var apiPageNumber = pageNumber - 1;
+      var requestUrl = '/nodes/search/findByServiceInstanceKey?key=' + $stateParams.key +
+          '&page=' + apiPageNumber +
+          '&size=' + paginationConfig.itemsPerPage +
+          '&sort=name' +
+          '&projection=serviceInstanceNodes';
 
-        var successHandler = function(res) {
-          vm.metadata = res.data.page;
-          vm.nodes = res.data._embedded.nodes;
+      var successHandler = function(res) {
+        console.log(res);
+        vm.metadata = res.data.page;
+        vm.nodes = res.data._embedded.nodes;
 
-          async.each(vm.nodes, function(node, cb) {
-            dataService.get(node._links.ipAddresses.href + '?projection=ipAddressDetails')
-              .then(function(res) {
-                node.ipAddresses = res.data._embedded.nodeIpAddresses;
-                async.each(node.ipAddresses, function(nIp, cb2) {
-                  dataService.get(nIp._links.aggregateRotationStatus.href + '?projection=rotationStatusDetails')
-                    .then(function(res) {
-                      nIp.aggregateRotationStatus = res.data;
-                      cb2();
-                    });
-                }, function(err) {
-                  if (err) {
-                    cb(err);
-                  } else {
-                    cb();
-                  }
-                });
+        async.each(vm.nodes, function(node, cb) {
+          dataService.get(node._links.ipAddresses.href + '?projection=ipAddressDetails')
+            .then(function(res) {
+              node.ipAddresses = res.data._embedded.nodeIpAddresses;
+              async.each(node.ipAddresses, function(nIp, cb2) {
+                dataService.get(nIp._links.aggregateRotationStatus.href + '?projection=rotationStatusDetails')
+                  .then(function(res) {
+                    nIp.aggregateRotationStatus = res.data;
+                    cb2();
+                  });
+              }, function(err) {
+                if (err) {
+                  cb(err);
+                } else {
+                  cb();
+                }
               });
-          }, function(err) {
-            if (err) return console.log(err);
-            var nodePage = vm.nodes;
-            vm.nodeRows = nodePageToNodeRows(nodePage);
-            vm.nodeListStatus = 'loaded';
-          });
-        };
+            });
+        }, function(err) {
+          if (err) return console.log(err);
+          var nodePage = vm.nodes;
+          vm.nodeRows = nodePageToNodeRows(nodePage);
+          vm.nodeListStatus = 'loaded';
+        });
+      };
 
-        var errorHandler = function(err) {
-          return console.log(err);
-        };
+      var errorHandler = function(err) {
+        return console.log(err);
+      };
 
-        dataService.get(requestUrl)
-          .then(successHandler, errorHandler);
-      }
+      dataService.get(requestUrl)
+        .then(successHandler, errorHandler);
     };
 
-    vm.nodes.pageSelected();
+    vm.pageSelected();
   }
 };
