8ab98cf48133788c30064253da82327167d83c7c
Ian D. McCunn <imccunn@expedia.com>
2016-02-23 13:40:04 -0800

    auth fix

--- server/auth.js
+++ server/auth.js
@@ -4,8 +4,45 @@ var passport = require('passport'),
     SamlStrategy = require('passport-saml').Strategy,
     authRoutes = require('./routes').auth;
 
-module.exports = function(app, router) {
+module.exports = function(app) {
+  var stratConfig = {
+    path: '/saml/consume',
+    protocol: config.auth.callbackProtocol + '://',
+    host: config.hostingEnvironment.externalHostname,
+    entryPoint: config.auth.identityProvider.spInitiatedUrl,
+    issuer: config.auth.serviceProvider.identity,
+    identifierFormat: config.auth.serviceProvider.desiredSubjectFormat,
+    cert: config.auth.identityProvider.signingCert,
+    acceptedClockSkewMs: config.auth.acceptableClockSkewInMs
+  };
+
+  function stratCb(profile, done) {
+    var profileObj = {
+      id: profile.NameID,
+      email: profile['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'],
+      displayName: profile.cn,
+      firstName: profile.givenName,
+      lastName: profile.sn,
+      department: profile.Department,
+      costCenter: profile['Cost Center'],
+      phone: profile['Phone']
+    };
+    return done(null, profileObj);
+  }
+
+  var authenticationStrategy = new SamlStrategy(stratConfig, stratCb);
+  passport.use(config.auth.strategy, authenticationStrategy);
+  app.use(passport.initialize());
+  app.use(passport.session());
 
 
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
 
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
+  authRoutes.init(app, passport, authenticationStrategy, config);
 };
--- server/index.js
+++ server/index.js
@@ -23,6 +23,7 @@ logger.format('access', logConfig.loggerFormat);
 app.use(logger('access', {stream: logConfig.accessLogStream}));
 
 app.use(cookieParser());
+
 var sessionConfig = {
   secret: config.sessionSecret,
   resave: false,
@@ -38,54 +39,12 @@ app.use(session(sessionConfig));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var stratConfig = {
-  path: '/saml/consume',
-  protocol: config.auth.callbackProtocol + '://',
-  host: config.hostingEnvironment.externalHostname,
-  entryPoint: config.auth.identityProvider.spInitiatedUrl,
-  issuer: config.auth.serviceProvider.identity,
-  identifierFormat: config.auth.serviceProvider.desiredSubjectFormat,
-  cert: config.auth.identityProvider.signingCert,
-  acceptedClockSkewMs: config.auth.acceptableClockSkewInMs
-};
-
-function stratCb(profile, done) {
-  var profileObj = {
-    id: profile.NameID,
-    email: profile['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'],
-    displayName: profile.cn,
-    firstName: profile.givenName,
-    lastName: profile.sn,
-    department: profile.Department,
-    costCenter: profile['Cost Center'],
-    phone: profile['Phone']
-  };
-  return done(null, profileObj);
-}
-
-var authenticationStrategy = new SamlStrategy(stratConfig, stratCb);
-
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(config.auth.strategy, authenticationStrategy);
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
-var authRouter = express.Router();
-authRoutes.init(authRouter, passport, authenticationStrategy, config);
-app.use('/', authRouter);
+configAuth(app);
 
 // Routes
 
-var internalRouter = express.Router();
-routes.internal(internalRouter);
-app.use('/', internalRouter);
+routes.internal(app);
+
 
 app.use('*', function(req, res, next) {
   next();
