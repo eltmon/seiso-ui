69f016353013a9b0b6e3856171000426f71010e7
Ian D. McCunn <imccunn@expedia.com>
2016-02-29 12:52:51 -0800

    update node alerts functionality/paging

--- client/components/service-instance/details/dashboard/node-alerts-controller.js
+++ client/components/service-instance/details/dashboard/node-alerts-controller.js
@@ -7,32 +7,36 @@ module.exports = function(app) {
   /* @ngInject */
   function nodeAlertsController(dataService, paginationConfig, $stateParams) {
     var vm = this;
+    vm.currentPage = 1;
+    vm.pageSelected = function() {
+      vm.nodeAlertsStatus = 'loading';
+      var pageNumber = vm.nodeAlerts.currentPage;
+      var apiPageNumber = pageNumber - 1;
+      var path = '/nodes/search/findNodeAlertsByServiceInstance?key=' + $stateParams.key + 
+          '&view=serviceInstanceNodes' + 
+          '&page=' + apiPageNumber + 
+          '&size=' + paginationConfig.itemsPerPage + 
+          '&sort=name';
+
+      dataService.get(path)
+        .then(successHandler, errorHandler);
+
+      function successHandler(res) {
+        vm.nodeAlertsPage = res.data._embedded;
+        vm.metadata = vm.nodeAlertsPage.metadata;
+        vm.nodeRows = nodePageToNodeRows(vm.nodeAlertsPage);
+        vm.nodeAlerts = vm.nodeAlertsPage.nodes;
+        vm.nodeAlertsStatus = 'loaded';
+      }
+      
+      function errorHandler() { vm.nodeAlertsStatus = 'error'; }
+    }
+
     vm.nodeAlerts = {
       currentPage: 1,
-      pageSelected: function() {
-        vm.nodeAlertsStatus = 'loading';
-        var pageNumber = vm.nodeAlerts.currentPage;
-        var apiPageNumber = pageNumber - 1;
-        var path = '/nodes/search/findNodeAlertsByServiceInstance?key=' + $stateParams.key + 
-            '&view=serviceInstanceNodes' + 
-            '&page=' + apiPageNumber + 
-            '&size=' + paginationConfig.itemsPerPage + 
-            '&sort=name';
-
-        var successHandler = function(res) {
-          vm.nodeAlertsPage = res.data._embedded;
-          vm.metadata = vm.nodeAlertsPage.metadata;
-          vm.nodeRows = nodePageToNodeRows(vm.nodeAlertsPage);
-          vm.nodeAlerts = vm.nodeAlertsPage.nodes;
-          vm.nodeAlertsStatus = 'loaded';
-        };
-        
-        var errorHandler = function() { vm.nodeAlertsStatus = 'error'; };
-
-        dataService.get(path)
-          .then(successHandler, errorHandler);
-      }
+      pageSelected: 
     };
-    vm.nodeAlerts.pageSelected();
+    
+    vm.pageSelected();
   }
 };
