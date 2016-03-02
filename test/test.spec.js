c45dbb196152df70b9c5844fc2ba06e3179ab570
Ian D. McCunn <imccunn@expedia.com>
2016-03-01 16:29:05 -0800

    update files to load into browsers for test, assure dep order

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,12 +1,18 @@
 'use strict';
 
-require('../build/static/js/build.bundle.js');
+require('../static/js/build.bundle.js');
 
-require('../node_modules/angular-mocks/angular-mocks.js');
+// require('../node_modules/angular-mocks/angular-mocks.js')
 
 describe('seiso-ui', function() {
   
   it('should be able to run tests', function testOne(done) {
+    expect(true).toBe(true);
+    expect(1).toBeTruthy();
+    done();
+  });
+
+    it('should be able to run tests', function testOne(done) {
     expect(true).toBe(true);
     expect(1).toBeTruthy();
     done();
@@ -24,11 +30,10 @@ describe('seiso-ui', function() {
     }));
 
     it('should be able to create a controller', function testTwo() {
-      var homeController = $ControllerConstructor('HomeController', {$scope: $scope});
+      var homeController = $ControllerConstructor('HomeController as vm', {$scope: $scope});
       expect(typeof homeController).toBe('object');
-      expect($scope.model.title).toBe('Home - Seiso');
-      expect($scope.serviceStatus).toBe('loading');
+      expect($scope.vm.serviceStatus).toBe('loading');
+      expect(typeof $scope.vm.getServices).toBe('function');
     });
   });
 });
-
