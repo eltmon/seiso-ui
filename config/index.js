dd8b101a2c55e52d7132d5fb9fa1e62626593f9c
Ian D. McCunn <imccunn@expedia.com>
2016-03-17 11:15:36 -0700

    reference config file

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
 
 var _ = require('lodash');
 
-var all = require('./env/_allll.js'),
+var all = require('./env/all.js'),
     envConfig = require('./env/' + (process.env.NODE_ENV || 'test') + '.js') || {},
     deployConfig = {},
     clientConfig = {clientConfig: require('./config.js')};
